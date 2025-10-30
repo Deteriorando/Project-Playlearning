1056b5757e6bc3dd5c5457cfdc4f194b2b3ab100
yasminfernandes-code <Fernandes.yasmin@aluno.ifsp.edu.br>
2025-10-29 22:37:52 -0300

    Animações via Código com AnimatedSprite2D

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
-
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 30.0
 const JUMP_VELOCITY = -300.0
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,5 +20,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	if is_on_floor():
+		#faz o personagem girar
+		if direction > 0:
+			anim.flip_h = false
+			anim.play("andando")
+		elif direction < 0:
+			anim.flip_h = true
+			anim.play("andando")
+		else:
+			anim.play("parado")
+	else:
+			anim.play("pulo")
 	move_and_slide()
