6eba62fb8c8162141b404850f422b6f3eb878106
yasminfernandes-code <Fernandes.yasmin@aluno.ifsp.edu.br>
2025-11-06 23:07:21 -0300

    obrigada deus. finalmente o reset esta funcionando

--- scripts/dialogo_box.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-
-#func _on_mata_dialogo_button_pressed() -> void:
-	#queue_free()
---
+++ scripts/killzone.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Corpo detectado: ", body.name) # Isso deve aparecer no painel Output/Saída
+	if body.is_in_group("pleyer"):
+		print("GRUPO PLEYER DETECTADO!") # Isso deve aparecer
+		get_tree().reload_current_scene()
