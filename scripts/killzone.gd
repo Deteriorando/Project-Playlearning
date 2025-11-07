cafd6269c3cfe3b51214fc0d3529294774aeca54
yasminfernandes-code <Fernandes.yasmin@aluno.ifsp.edu.br>
2025-11-06 23:10:21 -0300

    concertei um erro de reset

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body: Node2D) -> void:
 	print("Corpo detectado: ", body.name) # Isso deve aparecer no painel Output/Saída
 	if body.is_in_group("pleyer"):
 		print("GRUPO PLEYER DETECTADO!") # Isso deve aparecer
-		get_tree().reload_current_scene()
+		get_tree().call_deferred("reload_current_scene")
