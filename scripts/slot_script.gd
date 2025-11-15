30c338f2c7ac1d54aedb60a42a7694f95a19b823
yasminfernandes-code <Fernandes.yasmin@aluno.ifsp.edu.br>
2025-11-15 17:21:05 -0300

    inventario0.1

---
+++ scripts/slot_script.gd
@@ -0,0 +1,19 @@
+extends Control
+
+func _get_drag_data(at_position: Vector2):
+	var data = {"texture": $sprite_item.texture}
+	var preview = self.duplicate()
+	preview.get_node("Background").hide()
+	set_drag_preview(preview)
+	return data
+	
+	set_empty_slot()
+	
+func set_empty_slot()-> void:
+		$sprite_item.texture = null
+	
+func _can_drop_data(at_position: Vector2, data) -> bool:
+	return true
+	
+func _drop_data(position: Vector2, data) -> void:
+	$sprite_item.texture = data["texture"]
